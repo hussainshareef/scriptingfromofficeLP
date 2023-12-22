be6e6423a2fcd7bc2949a136f8f5403abf0cfb3d
shareef <hsmohammed@email.xtglobal.com>
2023-12-22 13:32:19 +0530

    change 2.0

--- hello.sh
+++ hello.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR: User is not a root"
+    echo "$R ERROR: User is not a root"
     exit 1
 else
-    echo "User is a root one"
+    echo "$G User is a root one"
 fi
