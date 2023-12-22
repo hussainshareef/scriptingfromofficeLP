dbf4187a68501d885bfa9ee12ece1ecffc662b0a
shareef <hsmohammed@email.xtglobal.com>
2023-12-22 13:33:32 +0530

    change 2.0

--- hello.sh
+++ hello.sh
@@ -7,8 +7,8 @@ N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR: User is not a root"
+    echo "$R ERROR: User is not a root $N"
     exit 1
 else
-    echo "$G User is a root one"
+    echo "User is a root one"
 fi
