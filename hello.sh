651605c0e57acdcff9f9aa81fe09b1daf30279b7
shareef <hsmohammed@email.xtglobal.com>
2023-12-22 12:55:32 +0530

    change 2.0

--- hello.sh
+++ hello.sh
@@ -2,6 +2,14 @@
 
 ID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H:%M:%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 if [ $ID -ne 0 ]
 then
     echo "$R ERROR $N : User is not a root" &>> $LOGFILE
