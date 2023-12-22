7149fa3afb37106ffa471603490c79ba5f70e4f9
shareef <hsmohammed@email.xtglobal.com>
2023-12-22 13:30:42 +0530

    change 2.0

--- hello.sh
+++ hello.sh
@@ -2,16 +2,7 @@
 
 ID=$(id -u)
 
-#R="\e[31m"
-#G="\e[32m"
-#Y="\e[33m"
-#N="\e[0m"
-
-#TIMESTAMP=$(date +%F-%H:%M:%S)
-#LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
 if [ $ID -ne 0 ]
-
 then
     echo "ERROR: User is not a root"
     exit 1
