f96e36ad8d34568a04685572a6b0e7e9d69a9228
shareef <hsmohammed@email.xtglobal.com>
2023-12-22 14:43:54 +0530

    change 2.0

--- hello.sh
+++ hello.sh
@@ -5,6 +5,10 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+echo "Script started executing at $TIMESTAMP"
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR: User is not a root $N"
