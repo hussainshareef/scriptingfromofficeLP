31c844e5401659e3a799798eb0295adab3b724cc
shareef <hsmohammed@email.xtglobal.com>
2023-12-28 10:43:53 +0530

    roboshop.sh

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-037a43886a4370096
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+
+
+for i in "${INSTANCES[@]}"
+    do
+        if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+        then
+            INSTANCE_TYPE="t3.small"
+        else
+            INSTANCE_TYPE="t2.micro"
+        fi
+    done
+aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
